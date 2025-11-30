61893a12938d45cd221c1f945469cb09802b6a1f
Rahul Kothuri <rahulkothuri67@gmail.com>
2025-11-30 11:22:34 +0530

    url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8008/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://13.203.209.183/api';
 
 const api = axios.create({
   baseURL: API_URL,
